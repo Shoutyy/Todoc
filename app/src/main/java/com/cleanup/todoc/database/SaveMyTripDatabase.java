31e1c623cea6c757a63c6b11cd6f1ce712541894
Benjamin Debray <shouty91600@hotmail.fr>
2020-06-26 16:28:42 +0200

    update

--- app/src/main/java/com/cleanup/todoc/database/SaveMyTripDatabase.java
+++ app/src/main/java/com/cleanup/todoc/database/SaveMyTripDatabase.java
@@ -9,9 +9,18 @@ import android.content.Context;
 import android.support.annotation.NonNull;
 import android.arch.persistence.room.Database;
 
+import com.cleanup.todoc.R;
 import com.cleanup.todoc.model.Project;
 import com.cleanup.todoc.model.Task;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.concurrent.Executors;
 
 @Database(entities = {Task.class, Project.class}, version = 1, exportSchema = false)
@@ -20,12 +29,16 @@ public abstract class SaveMyTripDatabase extends RoomDatabase {
     // --- SINGLETON ---
     private static volatile SaveMyTripDatabase INSTANCE;
 
+    private static Context activity;
+
     // --- DAO ---
     public abstract ProjectDao projectDao();
     public abstract TaskDao taskDao();
 
     // --- INSTANCE ---
     public static SaveMyTripDatabase getInstance(Context context) {
+        activity = context.getApplicationContext();
+
         if (INSTANCE == null) {
             synchronized (SaveMyTripDatabase.class) {
                 if (INSTANCE == null) {
@@ -49,7 +62,7 @@ public abstract class SaveMyTripDatabase extends RoomDatabase {
         }
         return INSTANCE;
     }
-
+    /*
     private static Callback prepopulateDatabase(){
         return new Callback() {
 
@@ -63,7 +76,7 @@ public abstract class SaveMyTripDatabase extends RoomDatabase {
                 contentValues.put("color", 0xFFEADAD1);
                 db.insert("Project", OnConflictStrategy.IGNORE, contentValues);
 
-                /*
+
                 ContentValues contentValues2 = new ContentValues();
                 contentValues.put("id", 2L);
                 contentValues.put("name", "Projet Lucidia");
@@ -76,7 +89,7 @@ public abstract class SaveMyTripDatabase extends RoomDatabase {
                 contentValues.put("color", 0xFFA3CED2);
                 db.insert("Project", OnConflictStrategy.IGNORE, contentValues3);
 
-                 */
+
 
 
 
@@ -84,5 +97,48 @@ public abstract class SaveMyTripDatabase extends RoomDatabase {
 
             }
         };
+    } */
+    /*
+    private static void fillWithStartingData(Context context) {
+        TaskDao dao = getInstance(context).taskDao();
+
+        JSONArray tasks = loadJSONArray(context);
+
+        try {
+
+            for (int i=0; i< tasks.length(); i++) {
+                JSONObject task = tasks.getJSONObject(i);
+
+                long taskID = id;
+                long projectID =  projectid
+                String taskName = task.getString("name");
+                String projectName = task.getString("project");
+
+                dao.insertTask(new Task(1, taskName, projectName, 10/20/2020));
+            }
+        } catch (JSONException e) {
+
+        }
+    }   */
+
+    private static JSONArray loadJSONArray(Context context) {
+        StringBuilder builder = new StringBuilder();
+        InputStream in = context.getResources().openRawResource(R.raw.tasks);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+        String line;
+
+        try {
+            while ((line = reader.readLine()) != null) {
+                builder.append(line);
+            }
+
+            JSONObject json = new JSONObject(builder.toString());
+
+            return json.getJSONArray("data");
+        } catch (IOException |  JSONException exeeption) {
+            exeeption.printStackTrace();
+        }
+        return null;
     }
 }
