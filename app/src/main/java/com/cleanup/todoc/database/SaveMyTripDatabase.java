660ad0692116023b31262d35c3ba05169b3e06eb
Benjamin Debray <shouty91600@hotmail.fr>
2020-06-30 13:59:21 +0200

    update

--- app/src/main/java/com/cleanup/todoc/database/SaveMyTripDatabase.java
+++ app/src/main/java/com/cleanup/todoc/database/SaveMyTripDatabase.java
@@ -9,18 +9,10 @@ import android.content.Context;
 import android.support.annotation.NonNull;
 import android.arch.persistence.room.Database;
 
-import com.cleanup.todoc.R;
+
 import com.cleanup.todoc.model.Project;
 import com.cleanup.todoc.model.Task;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.concurrent.Executors;
 
 @Database(entities = {Task.class, Project.class}, version = 1, exportSchema = false)
